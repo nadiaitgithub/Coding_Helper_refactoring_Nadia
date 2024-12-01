9f33df845e14363bab3a171e049703b5b5e3c620
Nadia <njahan2401@gmail.com>
2024-12-01 12:12:23 +0600

    unnecessary comments are removed from the code. code smell- line 9-11 line- 15-21 refactor- removed static and make it instanance variable split functionality into seperate methods

--- code_clone/CosineSimilarity.java
+++ code_clone/CosineSimilarity.java
@@ -6,25 +6,27 @@ import java.util.ArrayList;
 
 public class CosineSimilarity {
 
-    public static ArrayList<double[]> similarArray = new ArrayList<double[]>();
-   // public static ArrayList<Double> clone = new ArrayList<>();
+    private final List<double[]> similarityResults;
+    public CosineSimilarity(){
+        this.similarityResults = new ArrayList<>();
+    }
 
-    public void getCosinesimilarity() {
-        double similarity = 0;
-        for (int i = 0; i < CloneCheck.ProjectFileName1.size(); i++) {
-            System.out.println("");
-            double[] similar;
-            //int k = CloneCheck.ProjectFileName2.size();
-            similar = new double[CloneCheck.ProjectFileName2.size()];
-            int count = 0;
-            // similar = new double[TfIdfCalculate.tfidfvectorProject2.size() + 1];
+
+    public List<double[]>
+        calculatesSimilarity(List<String>
+          projectFileNames1, List<String>
+          projectFileNames2,
+        List<double[]> vectors1, List<double[]>vectors2){
+        for (int i = 0; i < ProjectFileNames1.size(); i++) {
+            double[] similarityRow = new double[projectFileNames2.size()];
+
             for (int j = 0; j < CloneCheck.ProjectFileName2.size(); j++) {
                 similarity = cosineSimilarity(TfIdfCalculate.tfidfvectorProject1.get(i), TfIdfCalculate.tfidfvectorProject2.get(j));
                 BigDecimal bd = new BigDecimal(similarity).setScale(2, RoundingMode.HALF_UP);
                 double getSimilar = bd.doubleValue();
                 System.out.println(CloneCheck.ProjectFileName1.get(i) + " vs " + CloneCheck.ProjectFileName2.get(j) + " " + getSimilar + "%");
                 similar[count] = similarity;
-             //   clone.add(similarity);
+
 
                 count++;
             }
@@ -46,7 +48,7 @@ public class CosineSimilarity {
         }
         project1magnitude = Math.sqrt(project1magnitude);
         project2magnitude = Math.sqrt(project2magnitude);
-        //   if(project1magnitude!=0 && project2magnitude!=0){
+
         cosinesimilarity = dotproduct / (project1magnitude * project2magnitude) * 100;
         if (Double.isNaN(cosinesimilarity)) {
             cosinesimilarity = 0.0;
@@ -55,16 +57,6 @@ public class CosineSimilarity {
 
     }
 
-   /* public void getAverage() {
-        double sum = 0;
-        for (int i = 0; i < clone.size(); i++) {
-          //  System.out.println(clone.get(i));
-            sum += clone.get(i);
-        }
-      
-        double average = sum /clone.size();
-     //   System.out.println(average);
 
-    }*/
 
 }
